6f96a7f546f50481d9f900188ad491e4d1f5d24c
Christian Anet Afonso de Andrade <90299478+ihchris@users.noreply.github.com>
2025-04-08 09:10:16 -0300

    Refactor library management system: implement Book, User, and Library classes; add lending functionality; update JSON handling for book data

--- main.py
+++ main.py
@@ -1,60 +1,14 @@
-class Book:
-    def __init__(self, title, author, ISBN):
-        self.title = title
-        self.author = author
-        self.ISBN = ISBN
-        self.available = True
+from models import Book, User, Library
+from operations import lend_book
+from utils import save_books, load_books
 
-
-class User:
-    def __init__(self, name, ID):
-        self.name = name
-        self.ID = ID
-        self.borrowed_books = []
-
-    def __str__(self):
-        return f"User: {self.name} (ID: {self.ID})"
-
-    def __repr__(self):
-        return f"<User('{self.name}', {self.ID})>"
-
-class Library:
-    def __init__(self):
-        self.books = {}
-
-    def add_book(self, book):
-            self.books[book.ISBN] = book
-    
-    def lend_book(self, isbn, user):
-        if isbn in self.books:
-            book = self.books[isbn]
-            if book.available:
-                book.available = False
-                user.borrowed_books.append(book)
-                print(f"{user.name} borrowed '{book.title}'")
-            else:
-                print("Book not available")
-        else:
-            print("Book not found.")
-
-# Creating library instance
 library = Library()
+book = Book("1984", "George Orwell", "123")
+user = User("Chris", 1)
 
-# Creating book instance
-book1 = Book("The Lord of the Rings", "J.R.R. Tolkien", "978-0-306-40615-7")
+library.books[book.ISBN] = book
+library.users[user.ID] = user
 
-# Creating user instance
-user1 = User("Christian", 1)
+lend_book(library, "123", user)
 
-# Adding book to library
-library.add_book(book1)
-
-# User borrowing book
-library.lend_book("978-0-306-40615-7", user1)
-
-# Checking the status of the book and user
-print("\n--- Current Status ---")
-print(f"Book available? {book1.available}")
-print(f"Books borrowed by {user1.name}: {[book.title for book in user1.borrowed_books]}")
-
-
+save_books(library.books, "data/books.json")
